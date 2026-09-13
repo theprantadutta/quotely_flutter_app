25e1079d834bd3a2053e436bfd6279c2223fbf30
PRANTA Dutta <pranta.dutta@kdsgroup.net>
2026-09-13 10:11:18 +0600

    Bump Gradle wrapper, AGP, and Kotlin
    Gradle     9.6.1  -> 9.7.1
    AGP        9.2.1  -> 9.4.0
    Kotlin     2.2.21 -> 2.3.20

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.7" apply false
